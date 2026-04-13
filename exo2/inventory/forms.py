d794d9f1ba422cb6232aafd6f5321c1e0b439232
w0lwin <teo.bacher@hotmail.com>
2026-04-13 15:39:26 +0200

    fix(exercice-2): simplify invoice creation form

--- exo2/inventory/forms.py
+++ exo2/inventory/forms.py
@@ -16,7 +16,7 @@ InvoiceItemFormSet = inlineformset_factory(
     Invoice,
     InvoiceItem,
     fields=['product', 'quantity'],
-    extra=1,
+    extra=0,
     can_delete=True,
     min_num=1,
     validate_min=True,
